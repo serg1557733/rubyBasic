005a50a7363930238d27b5efd9c969d91653b6f5
Serhii Borshch <72445490+serg1557733@users.noreply.github.com>
2024-06-06 13:28:56 +0200

    git testing

--- fun_translations/spec/lib/exchange_it/account_spec.rb
+++ fun_translations/spec/lib/exchange_it/account_spec.rb
@@ -3,8 +3,14 @@ RSpec.describe ExchangeIt::Account do
   let(:user_class) { Struct.new(:name, :surname) }
   let(:john) { described_class.new(user_class.new("John", "Doe")) }
 
-  it "has zero balance" do
+  it "has zero retro" do
     expect(john.balance).to eq(0)
+  end
+
+  it "has not uid" do
+    expect(john.uid).to be_an_instance_of(String)
+    hash = john.hash "John", "Doe"
+    expect(john.uid).to eq(hash)
   end
 end
 
